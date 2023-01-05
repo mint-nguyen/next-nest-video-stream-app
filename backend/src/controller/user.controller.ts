72e94d0aa54b225a0f8e7227f052cb56da77f753
mint-ng <mintnguyen.pa@gmail.com>
2023-01-05 14:20:16 -0800

    feat: sign up and sign in route

---
+++ backend/src/controller/user.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/model/user.schema';
+
+@Controller('/api/v1/user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('/signup')
+  async signup(@Res() response, @Body() user: User) {
+    const newUser = await this.userService.signup(user);
+    return response.status(HttpStatus.CREATED).json({ newUser });
+  }
+
+  @Post('/signin')
+  async SignIn(@Res() response, @Body() user: User) {
+    const token = await this.userService.signin(user, this.jwtService);
+    return response.status(HttpStatus.OK).json({ token });
+  }
+}
